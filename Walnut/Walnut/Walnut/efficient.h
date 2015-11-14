78c623bb7567dfdd16f4baf869f3603381a1a372
Dome P <me@rpdome.com>
2015-11-14 14:55:57 -0500

    vs2013

---
+++ Walnut/Walnut/Walnut/efficient.h
@@ -0,0 +1,35 @@
+#ifndef __EFFICIENT_H__
+#define __EFFICIENT_H__
+
+#include "sceneStructs.h"
+
+namespace StreamCompaction {
+namespace Efficient {
+
+    int compact(int n, RayState *idata);//, RayState *odata);
+}
+}
+
+inline int ilog2(int x) {
+    int lg = 0;
+    while (x >>= 1) {
+        ++lg;
+    }
+    return lg;
+}
+
+inline int ilog2ceil(int x) {
+    return ilog2(x - 1) + 1;
+}
+
+
+namespace StreamCompaction {
+namespace Common {
+    __global__ void kernMapToBoolean(int n, int *bools, const RayState *idata);
+
+    __global__ void kernScatter(int n, RayState *odata,
+            const RayState *idata, const int *bools, const int *indices);
+}
+}
+
+#endif
